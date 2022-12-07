71823303cc02bf9fc2654b3f8c05eb7e9b91cccc
Yujeong <lfyvon13@gmail.com>
2022-12-07 11:00:17 +0900

    [6kyu] Find the missing letter

---
+++ 6kyu/find-the-missing-letter.js
@@ -0,0 +1,28 @@
+// DESCRIPTION:
+// Find the missing letter
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+// (Use the English alphabet with 26 letters!)
+
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// I have also created other katas. Take a look if you enjoyed this kata!
+
+//My solution
+function findMissingLetter(array){
+    let letter = ''
+    for(let i = 0; i < array.length - 1; i++){
+      let num = array[i].charCodeAt() + 1
+      if(num != array[i + 1].charCodeAt()){
+        letter = String.fromCharCode(num);
+      }
+    }
+    return letter;
+}
