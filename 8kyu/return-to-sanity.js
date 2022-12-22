b4c84d6033b1c1e9804e02ffe6b576d2600c11c8
Yujeong <lfyvon13@gmail.com>
2022-12-22 23:33:55 +0900

    [8kyu] Return to Sanity

---
+++ 8kyu/return-to-sanity.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+//My solution
+function mystery() {
+    var results = {sanity: 'Hello'};
+    return results;
+}
